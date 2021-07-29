9deec349467c8aa2ba6a0d46c20cc561069fef71
abbeyroyse13 <heatherruan126@gmail.com>
2021-07-29 15:42:08 -0500

    attempting filtering thru category & by favorites (failing so far HAH)

--- src/components/horrorItems/horrorItemList.js
+++ src/components/horrorItems/horrorItemList.js
@@ -1,15 +1,27 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { HorrorItemContext } from "./horrorItemProvider"
 import { HorrorItemCard } from "./horrorItemCard"
 import "./horrorItem.css"
 
 export const HorrorItemList = () => {
     const { horrorItems, getHorrorItems } = useContext(HorrorItemContext)
+    // const [filterItems, setFilterItems] = useState([]);
 
     useEffect(() => {
         console.log("HorrorItemList: useEffect - getHorrorItems")
         getHorrorItems()
     }, [])
+
+    // const handleSortClick = (event) => {
+    //     event.preventDefault()
+    //     setFilterItems(
+    //         horrorItems.filter(item => {
+    //             if (item.categoryId === parseInt(event.target.id)) {
+    //                 return item
+    //             }
+    //         })
+    //     )
+    // }
 
     return (
         <div className="horrorItems">
@@ -23,3 +35,20 @@ export const HorrorItemList = () => {
         </div>
     )
 }
+
+// return (
+//     <div className="horrorItems">
+//         {console.log("HorrorItemList: Render", horrorItems)}
+//         <button onClick={handleSortClick}>Movies</button>
+//         <button onClick={handleSortClick}>Shows</button>
+//         <button onClick={handleSortClick}>Games</button>
+//         <button onClick={handleSortClick}>Books</button>
+//         <h2>Horror Items</h2>
+//         {
+//             filterItems.map(horrorItem => {
+//                 return <HorrorItemCard key={horrorItem.id} horrorItem={horrorItem} />
+//             })
+//         }
+//     </div>
+// )
+// }
